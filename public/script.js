7d062385f1568c911e88ff01ef7334cd225538c8
Galina Gatalskaya <gatalskaya.cniitu@gmail.com>
2024-12-05 14:41:47 +0300

    доработка дз
    доработка дз

--- public/script.js
+++ public/script.js
@@ -4,18 +4,24 @@ async function getPage() {
   const response = await fetch('/getListFiles');
   const listFiles = await response.json();
   const files = document.getElementById('listfiles');
+  files.innerHTML = '';
   listFiles.forEach((e) => {
     const div = document.createElement('div');
-    const btn = document.createElement('button');
+    const btn1 = document.createElement('button');
     const btn2 = document.createElement('button');
     const btn3 = document.createElement('button');
-    btn.innerText = `Файл: ${e.fileName}`;
+    const btn4 = document.createElement('button');
+    btn1.innerText = `Файл: ${e.fileName}`;
     btn2.innerText = 'Скачать';
     btn3.innerText = 'Добавить комментарий';
+    btn4.innerText = 'Просмотреть комментарий';
     btn2.onclick = () => fileDownload(e.fileName);
-    div.appendChild(btn);
+    btn3.onclick = () => addBlockComment(e.fileName);
+    btn4.onclick = () => showComment(e.fileName);
+    div.appendChild(btn1);
     div.appendChild(btn2);
     div.appendChild(btn3);
+    div.appendChild(btn4);
     files.appendChild(div);
   });
 }
@@ -24,8 +30,107 @@ async function fileDownload(fileName) {
   const response = await fetch('/download', {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json',
+      'Content-Type': 'text/plain',
     },
-    body: JSON.stringify({ fileName }),
+    body: fileName,
+  });
+  const data = await response.blob();
+  var fakebtn = document.createElement('a');
+  fakebtn.href = window.URL.createObjectURL(new Blob([data]));
+  fakebtn.download = fileName;
+  fakebtn.click();
+}
+
+const form = document.getElementById('uploadForm');
+form.onsubmit = (evt) => fileUpload(evt);
+
+async function fileUpload(evt) {
+  evt.preventDefault();
+  const form = document.getElementById('uploadForm');
+  const file = document.getElementById('file');
+  const fileProgress = document.getElementById('fileProgress');
+
+  const url = 'ws://nodearch.e-learning.by:5632';
+  // const url = 'ws://localhost:5632';
+  let connection = new WebSocket(url);
+  connection.onopen = (event) => {
+    console.log(
+      'Successfully CONNECTED to the echo websocket server...',
+      event
+    );
+  };
+  connection.onmessage = function(event) {
+    // console.log('клиентом получено сообщение от сервера: ' + event.data);
+    if (Number.isFinite(+event.data)) fileProgress.value = +event.data;
+  };
+  connection.onerror = function(event) {
+    console.log('websocket server error', event);
+  };
+  connection.onclose = function(event) {
+    console.log('websocket server closed', event);
+    connection = null;
+    clearInterval(keepAliveTimer);
+    fileProgress.value = 0;
+    getPage();
+  };
+  let keepAliveTimer = setInterval(() => {
+    connection.send('KEEP_ME_ALIVE');
+  }, 5000);
+
+  try {
+    await fetch('/upload', {
+      method: 'POST',
+      body: new FormData(form),
+    });
+    file.value = '';
+  } catch (error) {
+    console.error('Ошибка при загрузке файла:', error);
+  }
+}
+
+async function showComment(fileName) {
+  const labelForComment = document.getElementById('labelForComment');
+  labelForComment.classList.remove('hidden');
+  labelForComment.innerHTML = `Комментарий для ${fileName}`;
+  const comment = document.getElementById('comment');
+  comment.classList.remove('hidden');
+
+  const response = await fetch('/getComment', {
+    method: 'POST',
+    body: fileName,
+  });
+  const res = await response.text();
+  const commentText = JSON.parse(res).join('\n\n');
+  comment.value = commentText;
+  comment.disabled = true;
+}
+
+function addBlockComment(fileName) {
+  const labelForComment = document.getElementById('labelForComment');
+  labelForComment.innerHTML = `Здесь можно добавить комментарий для ${fileName}`;
+  labelForComment.value = fileName;
+  labelForComment.classList.remove('hidden');
+  const comment = document.getElementById('comment');
+  comment.value = '';
+  comment.classList.remove('hidden');
+  comment.disabled = false;
+  const btnAddComment = document.getElementById('btnAddComment');
+  btnAddComment.classList.remove('hidden');
+}
+
+async function addComment() {
+  const labelForComment = document.getElementById('labelForComment');
+  const comment = document.getElementById('comment');
+  const btnAddComment = document.getElementById('btnAddComment');
+  labelForComment.classList.add('hidden');
+  comment.classList.add('hidden');
+  btnAddComment.classList.add('hidden');
+
+  await fetch('/addComment', {
+    method: 'POST',
+    body: JSON.stringify({
+      fileName: labelForComment.value,
+      comment: comment.value,
+    }),
   });
 }
--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-// const multer = require('multer');
+const mime = require('mime/lite');
+const multer = require('multer');
+const WebSocket = require('ws');
 const bodyParser = require('body-parser');
 
 const webserver = express();
-// const upload = multer();
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, 'public'));
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage: storage });
 const port = 7380;
 
-// webserver.use(bodyParser.text({}));
+webserver.use(bodyParser.text({}));
 webserver.use(bodyParser.json({}));
 webserver.use(express.urlencoded({ extended: true }));
 webserver.use(express.static(path.resolve(__dirname, 'public')));
@@ -26,26 +37,97 @@ webserver.get('/getListFiles', (req, res) => {
   res.send(listFiles);
 });
 
-// webserver.post('/download', upload.none(), function(req, res) {
-webserver.post('/download', function(req, res) {
-  console.log('req.body', req.body);
-  // var file = __dirname + '/upload-folder/dramaticpenguin.MOV';
-
-  // var filename = path.basename(file);
-  // var mimetype = mime.getType(file);
-
-  // res.setHeader('Content-disposition', 'attachment; filename=' + filename);
-  // res.setHeader('Content-type', mimetype);
-
-  // var filestream = fs.createReadStream(file);
-  // filestream.pipe(res);
+webserver.post('/download', (req, res) => {
+  let filename = req.body;
+  let filePath = path.resolve(__dirname, 'public', filename);
+  let mimetype = mime.getType(filename);
+  res.setHeader('Content-disposition', 'attachment; filename=' + filename);
+  res.setHeader('Content-type', mimetype);
+  res.download(filePath, (err) => {
+    if (err) {
+      res.status(500).send('Ошибка при скачивании файла');
+    }
+  });
 });
 
-// webserver.post('/download', (req, res) => {
-//   res.setHeader('Content-Disposition', 'attachment; filename="fff.html"');
-//   res.setHeader('Content-Type', 'text/html');
+webserver.post('/upload', upload.single('file'), (req, res) => {
+  let clients = [];
+  let timer = 0;
+  const ws = new WebSocket.Server({ port: 5632 });
+  ws.on('connection', (connection) => {
+    connection.send('hello from server to client!');
+    let stats = 0;
+    let progress = 0;
+    let readStream = fs.createReadStream(req.file.originalname);
+    let writeStream = fs.createWriteStream(
+      path.join(__dirname, 'public', req.file.originalname)
+    );
+    readStream.pipe(writeStream);
 
-//   res.send('hello <b>goodbye</b>');
-// });
+    readStream.on('data', (chunk) => {
+      stats += chunk.length;
+      progress = Math.round((stats / req.file.size) * 100);
+      connection.send(progress);
+    });
+
+    connection.on('message', (message) => {
+      if (message === 'KEEP_ME_ALIVE') {
+        clients.forEach((client) => {
+          if (client.connection === connection)
+            client.lastkeepalive = Date.now();
+        });
+      } else console.log('сервером получено сообщение от клиента: ' + message);
+    });
+    clients.push({ connection: connection, lastkeepalive: Date.now() });
+
+    readStream.on('end', () => {
+      connection.close();
+      let arr = fs.readFileSync(
+        path.resolve(__dirname, 'public', 'list.json'),
+        'utf8'
+      );
+      let data = JSON.parse(arr);
+      data.push({ fileName: req.file.originalname, comments: [] });
+      fs.writeFileSync(
+        path.resolve(__dirname, 'public', 'list.json'),
+        JSON.stringify(data),
+        'utf8'
+      );
+      res.send('File uploaded successfully');
+    });
+  });
+
+  setInterval(() => {
+    timer++;
+    clients.forEach((client) => {
+      if (Date.now() - client.lastkeepalive > 12000) {
+        client.connection.terminate();
+        client.connection = null;
+      } else client.connection.send('timer=' + timer);
+    });
+    clients = clients.filter((client) => client.connection);
+  }, 3000);
+});
+
+webserver.post('/getComment', (req, res) => {
+  const listFN = path.resolve(__dirname, 'public', 'list.json');
+  const list = fs.readFileSync(listFN, 'utf8');
+  let listObj = JSON.parse(list);
+  let listItem = listObj.filter((e) => e.fileName === req.body);
+  const text = listItem[0].comments;
+  res.send(text);
+});
+
+webserver.post('/addComment', (req, res) => {
+  const body = JSON.parse(req.body);
+  const fileName = body.fileName;
+  const listFN = path.resolve(__dirname, 'public', 'list.json');
+  const list = fs.readFileSync(listFN, 'utf8');
+  let listObj = JSON.parse(list);
+  let listItem = listObj.filter((e) => e.fileName === fileName);
+  listItem[0].comments.push(body.comment);
+  fs.writeFileSync(listFN, JSON.stringify(listObj), 'utf8');
+  res.send('Комментарий успешно добавлен!');
+});
 
 webserver.listen(port, () => console.log('webserver running on port ' + port));
